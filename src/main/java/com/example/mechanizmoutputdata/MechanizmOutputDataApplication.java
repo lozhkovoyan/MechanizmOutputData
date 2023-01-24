b2362691dc3c8f109ca40944f91451c45ffb998a
jan <jan@MacBook-Pro-jan.local>
2023-01-24 18:00:26 +0300

    PRBLM-01 : add WebSecurityConfig.java for security, 'механизм не должен быть доступен кому попало'

---
+++ src/main/java/com/example/mechanizmoutputdata/MechanizmOutputDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.mechanizmoutputdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanizmOutputDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanizmOutputDataApplication.class, args);
+    }
+
+}
