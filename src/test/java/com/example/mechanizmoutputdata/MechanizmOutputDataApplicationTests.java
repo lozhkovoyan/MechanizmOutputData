1742ee507ad677a45e4a6b63b452773d8e36e59e
jan <jan@MacBook-Pro-jan.local>
2023-01-24 17:19:39 +0300

    PRBLM-01 : add Application.java: run , application.properties: parametrs for start PSql, pom: starter parents(web,jpa,security), lombok

---
+++ src/test/java/com/example/mechanizmoutputdata/MechanizmOutputDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mechanizmoutputdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanizmOutputDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
